5fb7799c5c11a305d3ecda2e22ac7d3a50538336
Fred Reimer <Fred.Reimer@jmfamily.com>
2018-01-28 11:01:46 -0500

    Bump version to 0.0.6

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ except ImportError:
 
 
 setup(name = 'AlertLogicAPI',
-    version = '0.0.5',
+    version = '0.0.6',
     description = 'Alert Logic API',
     long_description='Python interface to Alert Logic API.',
     author = 'Frederick Reimer',
